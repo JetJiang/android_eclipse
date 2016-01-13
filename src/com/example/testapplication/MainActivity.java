0524d5aed23ac0b1d8e13b8a6124c320cecfbca9
jiangjunjie <junjie5678@hotmail.com>
2016-01-13 22:04:52 +0800

    修改mainactivity

--- src/com/example/testapplication/MainActivity.java
+++ src/com/example/testapplication/MainActivity.java
@@ -4,7 +4,11 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
+/**
+ * 
+ * @author Administrator
+ *
+ */
 public class MainActivity extends ActionBarActivity {
 
 	@Override
